4e038fb28f2ee8b5b905c350656ba061778f1923
Vraj <vrajp8921@gmail.com>
2025-07-06 21:36:17 +0530

    Styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,36 +1,60 @@
 import React from 'react'
-import {Link} from 'react-router-dom' 
-import {Card, CardContent, Typography, CardMedia} from '@mui/material'
-import {CheckCircle} from '@mui/icons-material'
-import {demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl} from '../utils/constants'
+import { Link } from 'react-router-dom'
+import { Card, CardContent, Typography, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from '../utils/constants'
 
 
-const VideoCard = ({video:{id:{videoId},snippet}}) => {
-    console.log(snippet)
-    
+const VideoCard = ({ video }) => {
+  const videoId = video.id.videoId;
+  const videoDetails = video.snippet;
+  const channelId = video.snippet.channelId;
+  // console.log(videoId);
+
   return (
-    <Card>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-            <CardMedia 
-            image={snippet?.thumbnails?.high?.url}
-            alt={demoThumbnailUrl}
-            // sx={{width: '358px' , height: 180}}
-            sx={{width: {xs: '280px', sm: '358px', md: '320px'}, height: 180}}
-            
-            />
-        </Link>
-        <CardContent
-        sx={{ backgroundColor: "#1e1e1e", height: "106px"  }}
+    <Card
+      sx={{
+        width: { md: "330px", lg: "338px", sm: "320px", xs: "280px" },
+        boxShadow: 'none',
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        {/* <Link> */}
+        <CardMedia
+          image={videoDetails?.thumbnails?.high?.url}
+          alt={demoThumbnailUrl}
+          sx={{width: '358px' , height: 180}}
+          // sx={{ width: { xs: '280px', sm: '358px', md: '320px' }, height: 180 }}
+
         />
-         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-            <Typography
-              variant="subtital2"
-              fontWeight="bold"
-              color="#fff"
-            >
-              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-            </Typography>
-         </Link>
+      </Link>
+      <CardContent
+        sx={{ backgroundColor: "#1e1e1e", height: "106px" }}
+      >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          {/* <Link > */}
+          <Typography
+            variant="subtitle1"
+            fontWeight="bold"
+            fontSize={14}
+            color="#fff"
+          >
+            {videoDetails.title.slice(0, 54) || demoVideoTitle.slice(0, 54)}
+          </Typography>
+        </Link>
+        <Link to={channelId ? `/channel/${channelId}` : demoChannelUrl}>
+          <Typography
+            variant="subtitle2"
+            fontWeight="bold"
+            color="gray"
+          >
+            {videoDetails.channelTitle || demoChannelTitle}
+            
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+
+      </CardContent>
     </Card>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,10 +5,11 @@ import { VideoCard, ChannelCard } from './index'
 const Videos = ({videos}) => {
   
   return (
-    <Stack direction="row" className="flex-wrap justify-start gap-2">
+    <Stack direction="row" className="flex-wrap justify-start  gap-2">
      {videos.map((video,idx)=>(
-       <Box key={idx}>
-          {video.id.videoId && <VideoCard video={video}/>}
+      //  <Box key={idx}  sx={{  width: '300px', height: 'auto', backgroundColor: '#000',marginBottom: '5px', borderRadius: '10px', overflow: 'hidden'  }}>
+       <Box key={idx} >
+          {video&& <VideoCard video={video}/>}
           {/* {video.id.channelId && <ChannelCard channelDetail={video}/>} */}
       </Box>
       
